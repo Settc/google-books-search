d2d6ec5dd0463870351756fd22827453d1b160c2
Thomas Whittle <tewhittle@gmail.com>
2021-03-23 13:50:58 -0400

    Add nav bar component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Nav() {
+    return (
+        <nav class="pa3 pa4-ns">
+            <a class="link dim black b f6 f5-ns dib mr3" href="#" title="Home">Google Books Search</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="Home">Search</a>
+            <a class="link dim gray    f6 f5-ns dib mr3" href="#" title="About">Saved</a>
+            
+        </nav>
+
+    )
+}
+
+export default Nav
